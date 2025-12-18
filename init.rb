ec8f192739c5f447fb6546596cf2925fe9b7e455
Dmitry Ledentsov <dmitry.ledentsov@siemens.com>
2025-12-19 00:15:44 +0100

    move initializers into the plugin

--- init.rb
+++ init.rb
@@ -5,20 +5,16 @@
 
 require_relative 'lib/redmine_proxyauth/account_controller_patch'
 
-Redmine::Plugin.register :redmine_proxyauth do
-  name 'Redmine Proxyauth plugin'
-  author 'Alexander Vowinkel'
-  description 'Log in users via HTTP headers set by oauth2-proxy'
-  version '0.0.1'
-  url 'https://github.com/FES-Ehemalige/redmine_proxyauth'
-  author_url 'https://github.com/kaktus42'
+Redmine::Plugin.register :redmine_oauth2_proxy_auth do
+  name 'Redmine OAuth2 Proxy Authentication'
+  author 'Dmitry Ledentsov'
+  description 'Log in users via HTTP headers set by oauth2-proxy. Fork of redmine_proxyauth with enhanced GitHub OAuth2 support and seamless auto-login.'
+  version '0.1.0'
+  url 'https://github.com/d-led/redmine_oauth2_proxy_auth'
+  author_url 'https://github.com/d-led'
 
   requires_redmine version_or_higher: '5.1.0'
 
-  settings default: {
-    tenant_uri: '',
-    client_id: '',
-    client_secret: '',
-    tenant_id: '',
-  }, partial: 'settings/proxyauth_settings'
+  # No settings page needed - configuration is done via environment variables
+  # Original redmine_proxyauth had OIDC settings, but we use oauth2-proxy which handles OAuth2/OIDC
 end
